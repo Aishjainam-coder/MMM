c797fe10e2389bb4872213b27b13d8acd1704f27
Aishwarya Jain <jainaish.coder@gmail.com>
2024-06-07 02:30:22 +0530

    yes

--- weather app .py
+++ weather app .py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 win = Tk()
 
@@ -7,4 +8,51 @@ win.configure(bg="green")
 win.geometry("500x500")
 name_label = Label(win, text="MAUSAM DEKHO APP", font=("Times New Roman", 30, "bold"))
 name_label.place(x=25, y=50, height=50, width=450)
+
+list_name = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh ",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Lakshadweep",
+    "National Capital Territory of Delhi",
+    "Puducherry",
+]
+
+
+com = ttk.Combobox(
+    win, text="MAUSAM DEKHO APP", values=list_name, font=("Times New Roman", 30, "bold")
+)
+com.place(x=25, y=50, height=50, width=450)
+
+
 win.mainloop()
