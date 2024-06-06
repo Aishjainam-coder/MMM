4b8f688e6e56c51445246ab89dcb101a95937280
Aishwarya Jain <jainaish.coder@gmail.com>
2024-06-07 02:06:40 +0530

    first

---
+++ weather app .py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+win = Tk()
+
+win.title("MAUSAM DEKHO APP ")
+win.configure(bg="green")
+win.geometry("500x500")
+name_label = Label(win, text="MAUSAM DEKHO APP", font=("Times New Roman", 30, "bold"))
+name_label.place(x=25, y=50, height=50, width=450)
+win.mainloop()
